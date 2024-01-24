309ffc7800cb9ed32bd222d9ebc0ed068d2d1771
git <gitwazoku@gits-MacBook-Pro.local>
2024-01-24 07:37:14 -0500

    update mobile section2

--- src/app/components/home/Section2.jsx
+++ src/app/components/home/Section2.jsx
@@ -3,11 +3,11 @@ import Button from "../ui/Button.jsx";
 
 const Section2 = () => {
   return (
-    <div className="bg-lila p-32">
+    <div className="bg-lila p-16 md:p-32">
       <div className="mb-12">
       <Button variant="outlined" className="text-red-500">Securely trade your bets with other users</Button>
       </div>
-      <div className="flex flex-col md:flex-row space-between gap-16">
+      <div className="flex flex-col md:flex-row space-between gap-8 md:gap-16">
         <Card title="Título del Card" backgroundImage="/redeem.png" />
         <Card title="Título del Card" backgroundImage="/redeem.png" />
       </div>
@@ -17,7 +17,7 @@ const Section2 = () => {
 
 const Card = ({ title, backgroundImage }) => {
     return (
-      <div className="card h-96 w-1/2 rounded-lg overflow-hidden relative" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="card h-96 sm:w-full md:w-1/2 rounded-lg overflow-hidden relative" style={{ backgroundImage: `url(${backgroundImage})` }}>
         <span className="card__title absolute bottom-0 left-0 bg-white bg-opacity-75 p-2">{title}</span>
       </div>
     );
