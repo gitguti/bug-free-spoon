6f9ae963db565d89536464d13892388952341418
git <gitwazoku@gits-MacBook-Pro.local>
2024-01-24 21:31:24 -0500

    update section two images

--- src/app/components/home/Section2.jsx
+++ src/app/components/home/Section2.jsx
@@ -8,20 +8,23 @@ const Section2 = () => {
       <Button variant="outlined" className="text-red-500">Securely trade your bets with other users</Button>
       </div>
       <div className="flex flex-col md:flex-row space-between gap-8 md:gap-16">
-        <Card title="Título del Card" backgroundImage="/redeem.png" />
-        <Card title="Título del Card" backgroundImage="/redeem.png" />
+        <Card title="Transform your bet into a blockchain certified ticket" backgroundImage="/section21.jpg"     titleColor="text-white"/>
+        <Card title="Explore and trade on the marketplace" backgroundImage="/section22.jpg" titleColor="text-new-black" />
       </div>
     </div>
   );
 };
 
-const Card = ({ title, backgroundImage }) => {
-    return (
-      <div className="card h-96 sm:w-full md:w-1/2 rounded-lg overflow-hidden relative" style={{ backgroundImage: `url(${backgroundImage})` }}>
-        <span className="card__title absolute bottom-0 left-0 bg-white bg-opacity-75 p-2">{title}</span>
-      </div>
-    );
-  };
+const Card = ({ title, backgroundImage, titleColor }) => {
+  const titleClasses = `absolute top-6 left-6 p-2 text-3xl w-3/4 ${titleColor}`;
+
+  return (
+    <div className="card h-96 sm:w-full md:w-1/2 rounded-lg overflow-hidden relative" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <span className={titleClasses}>{title}</span>
+    </div>
+  );
+};
+
   
 
 export default Section2;
