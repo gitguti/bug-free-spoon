04fd8be9ef7ff3b1ec45b8584bea0eeb99a48ab2
git <gitwazoku@gits-MacBook-Pro.local>
2024-01-25 22:23:31 -0500

    section5

---
+++ src/app/components/home/Section5.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import Image from 'next/image';
+import Button from "../ui/Button";
+
+
+const Section5 = () => {
+  return (
+    <div className="bg-dirty-grey p-12 md:p-16 bg-[#EEF0FF] mx-auto">
+      <div className="mb-12 ">
+      </div>
+      <div className="flex gap-8 md:gap-16">
+        <Card />
+        <CTAApp/>
+      </div>
+      <div className="mx-auto w-5/12 my-12 text-center">
+  <h2 className="text-3xl md:text-5xl font-semibold leading-normal mb-4 text-gradient">
+    Make your bets accessible to players worldwide
+  </h2>
+  <div className="flex justify-center">
+    <Button variant="filled">Go to marketplace</Button>
+  </div>
+</div>
+
+    </div>
+
+  );
+};
+
+const Card = ({ title, backgroundImage, titleColor, description, backgroundColor, image, width, height }) => {
+
+  return (
+    <>
+      <div className=" px-7 py-10 h-96 w-1/3 rounded-lg overflow-hidden justify-content-start" style={{ backgroundColor: "#492C75" }}>
+        <p className="text-2xl font-bold mt-4 mb-4">Get the Mobile App</p>
+        <p className=" font-bold mt-4 mb-4">Key features of our app:</p>
+        <ul>
+          <li>Get real-time notifications when buyers are interested in your bet</li>
+          <li>Be the first to know when a bet is available for trade</li>
+        </ul>
+
+        <Image src="/downloadIOS.png" alt="Icon" width={200} height={60} />
+
+      </div>
+    </>
+
+  );
+};
+
+const CTAApp = ({}) => {
+  return (
+    <>
+    <div className="w-2/3 bg-cta-app bg-cover	h-96 rounded-lg">
+    </div>
+    </>
+  )
+}
+
+
+
+export default Section5;
