a80681379ce7cac1119cfb51328b18b003b65d67
git <gitwazoku@gits-MacBook-Pro.local>
2024-01-30 23:04:14 -0500

    dynamic component and featureInfo object

--- src/app/components/features/FeaturesCard.jsx
+++ src/app/components/features/FeaturesCard.jsx
@@ -2,17 +2,17 @@
 import React from 'react';
 import Image from 'next/image'; // Asumiendo que estás utilizando Next.js
 
-const FeaturesCard = ({ title, description, imageSrc }) => {
+const FeaturesCard = ({ title, description, image }) => {
   return (
     <>
 <div className="relative bg-another-grey px-10 py-12 flex flex-col w-96 h-96 m-48 rounded-lg border-t-8 border-purple transition-all duration-500 ease-in-out hover:before:h-full before:bg-purple-500 overflow-hidden featuresCard">
-  <div className="font-tomato text-2xl md:text-3xl 2xl:text-4xl font-semibold leading-normal mb-4 text-new-black">Easy Process</div>
+  <div className="font-tomato text-2xl md:text-3xl 2xl:text-4xl font-semibold leading-normal mb-4 text-new-black">{title}</div>
   <div className=" text-sm lg:text-base 2xl:text-lg mb-8 2xl:text-md text-new-black">
-    <p>Seamless one-click process to transform your bets into NFTs</p>
+    <p>{description}</p>
   </div>
   <div className="opacity-25">
     <Image
-      src="/footerImage.png"
+      src={image}
       alt="Logo de la compañía"
       className="w-10/12 absolute bottom-0 right-0"
       width={800}
--- src/app/features/page.js
+++ src/app/features/page.js
@@ -3,14 +3,51 @@ import React from 'react';
 import FeaturesCard from '../components/features/FeaturesCard';
 
 export default function Features() {
+  const featureInfo = [
+    {
+      title: "Easy Process",
+      description: "Seamless one-click process to transform your bets into NFTs",
+      image: "/footerImage.png",
+      hoverColor: "bg-purple-500" // Ejemplo de clase de color para el hover
+    },
+    {
+      title: "Ownership",
+      description: "Mint your bet as an NFT for exclusive ownership and undeniable proof in your crypto wallet.",
+      image: "/footerImage.png",
+      hoverColor: "bg-purple-500" // Ejemplo de clase de color para el hover
+    },
+    {
+      title: "Secure",
+      description: "Each NFT represents a verified bet from licensed sportsbooks, ensuring authenticity and confidence",
+      image: "/footerImage.png",
+      hoverColor: "bg-purple-500" // Ejemplo de clase de color para el hover
+    },
+    {
+      title: "Privacy",
+      description: "Trade NFT bets anonymously in our secure marketplace, prioritizing your privacy and anonymity",
+      image: "/footerImage.png",
+      hoverColor: "bg-purple-500" // Ejemplo de clase de color para el hover
+    },
+    // Repite el objeto para las otras tarjetas, cambiando los valores según sea necesario
+  ];
+  
   return (
     <>
  <div className="mb-12">
         <h2 className="text-3xl md:text-5xl 2xl:text-7xl font-semibold leading-normal mb-7 md:mb-14 text-gradient">Features</h2>
       </div>
-      <div>
-        <FeaturesCard/>
-      </div>
+      <div className="grid grid-cols-3">
+  {featureInfo.map((card, index) => (
+    <FeaturesCard
+      key={index}
+      title={card.title}
+      description={card.description}
+      image={card.image}
+      hoverColor={card.hoverColor}
+    />
+  ))}
+</div>
+
 
     </>
   );
