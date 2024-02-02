a8c9f99dafa94367bafe0b01e1d13eadaa1b8d9c
git <gitwazoku@gits-MacBook-Pro.local>
2024-02-02 01:43:53 -0500

    create section2 component

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Faqs from '../components/about/faqs';
+import Section2 from '../components/about/Section2';
 
 export default function About() {
 
@@ -99,11 +100,8 @@ export default function About() {
 </div>
 
     </div>
-    <div className='bg-lila pt-48 text-center flex flex-col justify-center mx-auto space-between gap-8'>
-      <div className='w-1/3 mx-auto text-2xl font-normal text-new-black'> <p>For these and many other reasons, whe have created Betsell, a platform that allows gamblers to sell their bets to other players in a secure and privacy preserving marketplace</p>
-</div>
-      <div className='lg:max-w-mxl mx-auto'>      <Image src="/about.png" width={800} height={300}/></div>
-      </div>
+    <Section2/>
+
       <div
     x-data="{}"
     x-init="$nextTick(() => {
