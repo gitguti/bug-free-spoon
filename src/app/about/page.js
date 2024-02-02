6de2d31f5b76fa0cc942d024b3bf9039258f498f
git <gitwazoku@gits-MacBook-Pro.local>
2024-02-01 21:34:40 -0500

    add section1 content about us

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,8 +1,32 @@
 
 export default function About() {
   return (
-   <main>
-    <div>hola</div>
-   </main>
+    <>
+    <div className="flex">
+      <div className="w-1/2">
+      <h2 className="font-tomato text-3xl md:text-6xl 2xl:text-8xl font-semibold leading-normal mb-4 text-gradient">Why should you consider trading your bets?</h2>
+      </div>
+      <div className="w-1/2 grid grid-cols-2 gap-3">
+      <div className="px-10 py-8 bg-purple  rounded-lg">
+        <p className="text-new-white ">Do you have an early future bet with improved and appealing odds compared to the current market conditions?</p>
+      </div>
+      <div className="px-10 py-8 bg-[#87ABF1] rounded-lg">
+        <p className="text-new-white ">Would you like to sell your original stake at a discounted price to clear it from your account?</p>
+      </div>
+      <div className="px-10 py-8 bg-[#CDC0E9] rounded-lg">
+        <p className="text-new-black ">Is your pending parlay bet just a few legs away from the potential to win a significant payout?</p>
+      </div>
+      <div className="px-10 py-8 bg-[#F4F4F4] rounded-lg">
+        <p className="text-new-black ">Do you wish to cash out early on your bet securing a guaranteed win?</p>
+      </div>
+      <div className="px-10 py-8 bg-[#907BCB] rounded-lg">
+        <p className="text-new-white ">Have the odds for your pending bet undergone drastic changes in the market, now favouring your original stake?</p>
+      </div>
+      <div className="px-10 py-8 bg-purple rounded-lg">
+        <p className="text-new-white ">Do you have an early future bet with improved and appealing odds compared to the current market conditions?</p>
+      </div>
+      </div>
+    </div>
+    </>
   );
 }
