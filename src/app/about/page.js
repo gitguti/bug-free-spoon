b4a8f281b1478184f3c112500705c751b9c4cedd
git <gitwazoku@gits-MacBook-Pro.local>
2024-02-02 01:40:05 -0500

    section 2 about us

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -99,9 +99,10 @@ export default function About() {
 </div>
 
     </div>
-    <div className='bg-lila h-10rem text-center flex flex-col justify-center mx-auto lg:max-w-md'>
-        <p>For these and many other reasons, whe have created Betsell, a platform that allows gamblers to sell their bets to other players in a secure and privacy preserving marketplace</p>
-      <Image src="/about.png" width={800} height={300}/>
+    <div className='bg-lila pt-48 text-center flex flex-col justify-center mx-auto space-between gap-8'>
+      <div className='w-1/3 mx-auto text-2xl font-normal text-new-black'> <p>For these and many other reasons, whe have created Betsell, a platform that allows gamblers to sell their bets to other players in a secure and privacy preserving marketplace</p>
+</div>
+      <div className='lg:max-w-mxl mx-auto'>      <Image src="/about.png" width={800} height={300}/></div>
       </div>
       <div
     x-data="{}"
