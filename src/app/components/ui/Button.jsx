9227175506a4d111f61ddb326ccf10631bab9214
git <gitmelgtrrz1@gmail.com>
2024-02-07 19:08:02 -0500

    added onClick to betsell.io

--- src/app/components/ui/Button.jsx
+++ src/app/components/ui/Button.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
 const Button = ({ children, variant }) => {
+  const handleClick = () => {
+    // Redirige a la URL deseada
+    window.location.href = 'https://betsell.io';
+  };
+
   const buttonClasses = `rounded-lg text-sm lg:text-base 2xl:text-lg px-6 py-4 focus:outline-none transition transition-colors transition-opacity duration-300 ${
     variant === 'filled'
       ? 'bg-new-black hover:bg-new-black hover:opacity-75 text-white'
       : 'button-outlined text-new-black'
   }`;
 
-  return <button className={buttonClasses}>{children}</button>;
+  return <button className={buttonClasses}  onClick={handleClick}>{children}</button>;
 };
 
 export default Button;
