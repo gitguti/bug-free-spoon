02b8f631e11d18d9585020e9dffe2b5f1dcbf872
git <gitmelgtrrz1@gmail.com>
2024-02-08 19:09:57 -0500

    update get mobile app section

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -130,8 +130,8 @@ export default function About() {
 
 </div>
 </div>
-<div className='bg-lila '>
-<div className="mx-auto w-11/12 md:w-9/12 2xl:w-8/12  py-16 lg:py-48 text-center">
+<div className='bg-lila px-4 md:px-8'>
+<div className="mx-auto sm:w-11/12 lg:w-8/12 xl:w-1/2 3xl:w-2/5  py-16 lg:py-32 text-center">
       <h2 className="font-tomato text-3xl md:text-4xl lg:text-5xl 2xl:text-7xl font-semibold leading-8 md:leading-normal my-8 text-gradient md:max-w-screen-md lg:max-w-screen-lg mx-auto">
         Make your bets accessible to players worldwide
       </h2>
--- src/app/components/home/Section5.jsx
+++ src/app/components/home/Section5.jsx
@@ -6,36 +6,26 @@ import Button from "../ui/Button";
 const Section5 = () => {
   return (
     <>
-      <div className="bg-lila mx-auto">
-      <div className="px-8 w-full md:px-0 md:w-10/12 xl:max-w-screen-3xl  py-16 lg:py-48  mx-auto xl:justify-center">
+      <div className="px-4 md:px-8 py-16 md:py-32  bg-lila mx-auto">
       <div
-        className="flex overflow-x-scroll lg:overflow-x-auto pb-10 scrollbar-hide md:hidden">
+        class="flex overflow-x-scroll pb-10 scrollbar-hide">
         <div
-          className="flex flex-nowrap w-full h-full gap-4 md:gap-4 md:justify-center">
-          <div className="inline-block">
+          class="flex flex-nowrap lg:justify-center w-full gap-3 sm:gap-4 md:gap-6">
+          <div class="inline-block">
             <Card />
           </div>
-          <div className="inline-block">
+          <div class="inline-block">
             <CTAApp />
           </div>
         </div>
       </div>
-      <div className="hidden md:grid w-full h-full grid-cols-3 gap-4 md:max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg mx-auto 2xl:max-w-screen-3xl">
-        <div className='col-start-1'>
-      <Card />
-      </div>
-      <div className='h-full col-start-2 col-span-2'>
-      <CTAApp />
-      </div>
-      </div>
-      <div className="mx-auto w-11/12 md:w-9/12 2xl:w-8/12 my-12 text-center">
-      <h2 className="font-tomato text-3xl md:text-4xl lg:text-5xl 2xl:text-7xl font-semibold leading-8 md:leading-normal my-8 text-gradient">
+      <div className="mx-auto sm:w-11/12 lg:w-8/12 xl:w-1/2 3xl:w-2/5 my-12 text-center">
+      <h2 className="text-3xl md:text-5xl 3xl:text-7xl font-semibold leading-9 md:leading-normal 3xl:leading-[4.6rem]  my-8 text-gradient">
         Make your bets accessible to players worldwide
       </h2>
       <div className="flex justify-center">
         <Button variant="filled">Go to marketplace</Button>
         </div>
-      </div>
       </div>
       </div>
     </>
@@ -46,17 +36,15 @@ const Section5 = () => {
 const Card = ({ }) => { 
 return (
         <>
-          <div className="w-full h-full px-8 py-4 max-w-xs md:max-w-md lg:max-w-screen-md 2xl:max-w-2xl overflow-hidden rounded-xl  bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out  justify-content-start" style={{ backgroundColor: "#492C75" }}>
-            <p className="font-tomato text-2xl lg:text-3xl 2xl:text-5xl font-bold mt-4 mb-4 text-gradient2">Get the Mobile App</p>
-            <div className='md:hidden lg:flex flex-col'>
+          <div className="min-w-64 max-w-80 h-full 3xl:min-w-[32rem] px-6 md:px-8 py-4 3xl:px-16 3xl:py-12 rounded-xl  transition-shadow duration-300 ease-in-out 3xl:flex 3xl:flex-col  3xl:justify-between " style={{ backgroundColor: "#492C75" }}>
+            <p className="text-2xl md:text-3xl 3xl:text-5xl  font-bold mt-4 mb-4 text-gradient2">Get the Mobile App</p>
             <p className=" text-[#c4c4c4] mt-4 mb-4 text-xs md:text-base 2xl:text-xl">Key features of our app:</p>
-            <ul className='text-[#fafafa] text-xs md:text-sm 2xl:text-lg mb-8 list-disc'>
+            <ul className='text-[#fafafa] text-xs md:text-sm 2xl:text-base 3xl:text-2xl  leading-normal mb-8 list-disc'>
               <li>Get real-time notifications when buyers are interested in your bet</li>
               <li>Be the first to know when a bet is available for trade</li>
             </ul>
-            </div>
 
-            <Image src="/downloadIOS.png" alt="Icon" width={200} height={60} />
+            <Image src="/downloadIOS.png" alt="Icon" width={250} height={60} />
 
           </div>
         </>
@@ -67,12 +55,7 @@ return (
         const CTAApp = ({ }) => {
   return (
         <>
-          <div className="relative h-full w-full hover:shadow-xl transition-shadow duration-300 ease-in-out object-fit-cover">
-          <Image
-        src="/CTAApp.png"
-        alt="Descripción"
-        fill
-      />
+          <div className="min-w-96 md:min-w-[40rem] min-h-full px-12 py-10 3xl:w-[80rem] 3xl:h-[36rem] overflow-hidden rounded-xl shadow-md bg-cta-app bg-cover  hover:shadow-xl transition-shadow duration-300 ease-in-out">
           </div>
         </>
         )
