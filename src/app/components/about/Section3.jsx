e94c4eae69ad6e89f0f88312b17658ae9cd4d9e6
gitguti <gitmelgtrrz1@gmail.com>
2024-02-16 08:41:08 -0500

    about fixed at 2540

--- src/app/components/about/Section3.jsx
+++ src/app/components/about/Section3.jsx
@@ -7,7 +7,7 @@ const Section3 = () => {
              <div className='font-tomato text-3xl md:text-4xl lg:text-5xl 2xl:text-7xl font-semibold leading-normal my-4 text-gradient'>
              <h2>About Betsell</h2>
              </div>
-            <div className="px-4 py-2 md:px-0 md:w-3/5 xl:w-3/5 3xl:w-4/5 xl:py-4 3xl:px-72 4xl:px-96 overflow-hidden mx-auto ">
+            <div className="px-4 py-2 md:px-0 md:w-3/5 xl:w-3/5 3xl:w-4/5 4xl:w-8/12 xl:py-4 3xl:px-72 4xl:px-96 overflow-hidden mx-auto ">
 
                 <div className='mx-auto bg-white rounded-xl'>      <Image priority={false}  src="/aboutPlaceholder.png" width={600} height={300} className='w-3/4 sm:w-2/5 md:w-[70%] xl:w-1/2 mx-auto pt-16 xl:pt-36' /></div>
                 </div>
