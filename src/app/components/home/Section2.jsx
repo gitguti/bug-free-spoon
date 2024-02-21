a4bf5fe2d0d2ab682179b3281b3edfab99e8fc64
gitguti <gitmelgtrrz1@gmail.com>
2024-02-20 22:24:13 -0400

    fixed fonts 0.2 rem less

--- src/app/components/home/Section2.jsx
+++ src/app/components/home/Section2.jsx
@@ -35,7 +35,7 @@ const Section2 = () => {
       <div className="bg-lila px-px sm:px-8 md:px-0 py-12 lg:py-24 4xl:py-36 mx-auto w-11/12 4xl:w-9/12">
         <div className="mb-6 md:mb-8 xl:mb-12 3xl:mb-10 4xl:mb-20 flex items-start md:items-center">
           <SwitchHorizontal />
-          <p className="pl-4 sm:text-xs md:text-base lg:text-2xl xl:text-[1.8rem] 4xl:text-[2.5rem] text-new-black font-tomato inline-block align-left md:align-center">Securely trade your bets with other users</p>
+          <p className="pl-4 sm:text-xs md:text-base lg:text-2xl xl:text-[1.8rem] 3x:text-[2.3rem] 4xl:text-[2.5rem] text-new-black font-tomato inline-block align-left md:align-center">Securely trade your bets with other users</p>
         </div>
         <div className="flex flex-col md:flex-row space-between gap-4 md:gap-[1.5rem]">
           <Card title="Transform your bet into a blockchain certified ticket" backgroundImage="/section21.webp" titleColor="text-white" isVisible={isVisible} />
