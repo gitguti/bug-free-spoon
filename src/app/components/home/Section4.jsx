10e1237f81c92e673911f751bf61dce56d66fb36
gitguti <gitmelgtrrz1@gmail.com>
2024-02-29 00:22:06 -0400

    hiw section

--- src/app/components/home/Section4.jsx
+++ src/app/components/home/Section4.jsx
@@ -10,11 +10,11 @@ const Section4 = () => {
         <h2 className="text-[3.5rem] md:text-[6.5rem] 2xl:text-[8.75rem] text-gradient font-semibold leading-none xl:leading-normal text-center text-gradient font-tomato">How it works
         </h2>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4 md:gap-6 2xl:gap-8">
-        <Card title="1. Pick" description="Choose the bet you wish to mint by simply clicking a button" titleColor="text-new-black" backgroundColor="#CDC0E9" image="/Pick.png" width="170" height="50" />
-        <Card title="2. Mint" description="Transform your bet into a certified asset in form of an NFT" titleColor="text-new-black" backgroundColor="#F4F4F4" image="/hiw2.png" width="70" height="70" />
-        <Card title="3. Trade" description="Your NFT is now ready to be traded on the global marketplace" titleColor="text-new-white" backgroundColor=" #2B213E" image="/hiw3.png" width="70" height="70" />
-        <Card title="4.Redeem" description="Get your payout effortlessly in the ‘redeem’ section of your sportsbook" titleColor="text-new-white" backgroundColor=" #6D5E8B" image="/hiw4.png" width="70" height="70" />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6 2xl:gap-8">
+        <Card title="1. Pick" description="From the open bets section of your participant Sportsbook account, choose the bet you wish to mint by simply clicking on a button" titleColor="text-new-black" backgroundColor="#CDC0E9" image="/Pick.png" width="170" height="50" />
+        <Card title="2. Mint" description="The  BetSell minting process will be executed, seamlessly transfroming your selected bet into a unique and certified asset in the form of an NFT. The newly minted NFT will then be allocated in the crypto wallet o your choice and automatically listed on BetSell’s Marketplace." titleColor="text-new-black" backgroundColor="#F4F4F4" image="/hiw2.png" width="70" height="70" />
+        <Card title="3. Trade" description="Your minted bet is now available on the BetSell Marketplace, enabling individuals  from around the world to buy,sell and make offers on your NFT." titleColor="text-new-white" backgroundColor=" #2B213E" image="/hiw3.png" width="70" height="70" />
+        <Card title="4.Redeem" description='NFT bets are digital certified tickets, ready to be cashed by their holders. By simply entering the "Redeem" section of the corresponding sportsbook, the holder can easily receive the payout deposited into their sportsbook account without any further inquiries.' titleColor="text-new-white" backgroundColor=" #6D5E8B" image="/hiw4.png" width="70" height="70" />
       </div>
     </div>
     </div>
@@ -29,12 +29,12 @@ const Card = ({ title, backgroundImage, titleColor, description, backgroundColor
 
   return (
     <>
-      <div className="px-7 py-10 h-72 w-full 2xl:h-96 rounded-lg 2xl:rounded-2xl overflow-hidden justify-content-start" style={{ backgroundColor: `${backgroundColor}` }}>
+      <div className="px-7 py-10 w-full h-[30rem] sm:h-96 3xl:h-[30rem] rounded-lg 2xl:rounded-2xl overflow-hidden flex flex-col justify-center sm:justify-end" style={{ backgroundColor: `${backgroundColor}` }}>
         <div className="h-[79.8633px]">
         <Image src={image} priority={true} alt="Icon" width={width} height={height} />
         </div>
         <p className={`${titleColor}  font-tomato text-2xl 2xl:text-4xl font-semibold mt-4 mb-2 xl:mt-8`}>{title}</p>
-        <p className={`${titleColor} text-base 2xl:text-2xl font-normal`}>{description}</p>
+        <p className={`${titleColor} text-base 2xl:text-2xl font-normal h-36 md:h-44 lg:h-32`}>{description}</p>
       </div>
     </>
 
