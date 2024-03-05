43ac7b5cc80362f564a03a1aa42c879442ec5d7f
gitguti <gitmelgtrrz1@gmail.com>
2024-03-05 17:57:01 -0400

    update font size for cards section2 and 4

--- src/app/components/home/Card.jsx
+++ src/app/components/home/Card.jsx
@@ -13,13 +13,13 @@ const Card = ({ title, backgroundImage, titleColor, delay, isVisible, content, b
     : '';
 
     const titleClasses = `absolute top-4 xl:top-[2.2rem] left-4 md:top-6 md:left-6 xl:left-[2.2rem] text-xl md:text-base lg:text-2xl xl:text-[2rem] xl:leading-[2.3rem] 3xl:text-[2.3rem] 3xl:leading-[2.9rem] 4xl:text-[2.9rem] 4xl:leading-[3.4rem] 2xl:p-8 w-4/5 lg:w-1/2 lg:w-2/3 xl:w-3/4 3xl:w-4/5 font-tomato font-medium ${titleColor}`;
-    const contentClasses = `absolute font-regular top-20 lg:top-24 xl:top-28 3xl:top-36 4xl:top-40 left-4 md:left-6 xl:left-[2.2rem] text-xs md:text-sm md:leading-[22px] xl:text-base 3xl:text-xl  4xl:text-[1.75rem] 4xl:leading-[2.1rem] 2xl:p-8 w-3/4 lg:w-1/2 lg:w-2/3 xl:w-3/4 3xl:w-4/5 ${titleColor}`;
+    const contentClasses = `absolute font-regular top-20 lg:top-24 xl:top-28 3xl:top-36 4xl:top-40 left-4 md:left-6 xl:left-[2.2rem] text-sm md:leading-[22px] xl:text-base 3xl:text-xl  4xl:text-[1.75rem] 4xl:leading-[2.1rem] 2xl:p-8 w-3/4 lg:w-1/2 lg:w-2/3 xl:w-3/4 3xl:w-4/5 ${titleColor}`;
   
     return (
       <div className={`card lg:w-1/2 rounded-lg 2xl:rounded-2xl overflow-hidden bg-cover bg-bottom relative opacity-0 ${animationClass} ${mobileAnimationClass}`} style={{ backgroundImage: `url(${backgroundImage})` }}>
         <p className={`${titleClasses}`}>{title}</p>
         <p className={`${contentClasses}`}>{content}</p>
-        <div className="absolute left-4 md:left-6 xl:left-8 3xl:left-16 top-48 sm:top-44 md:top-56 3xl:top-[19rem] 4xl:top-96">
+        <div className="absolute left-4 md:left-6 xl:left-8 3xl:left-16 top-52 sm:top-44 md:top-56 3xl:top-[19rem] 4xl:top-96">
         {button ? <Button variant="filled" >Go to marketplace</Button>: ''}
         </div>
 
--- src/app/components/home/Section4.jsx
+++ src/app/components/home/Section4.jsx
@@ -10,7 +10,7 @@ const Section4 = () => {
         <h2 className="text-3xl md:text-5xl 2xl:text-7xl font-semibold leading-none xl:leading-normal text-gradient3 font-tomato">How it works
         </h2>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="grid auto-cols-fr md:grid-cols-2 gap-4">
         <Card title="1. Pick" description="From the open bets section of your participant Sportsbook account, choose the bet you wish to mint by simply clicking on a button" titleColor="text-new-black" backgroundColor="#CDC0E9" image="/Pick.png" width="170" height="50" />
         <Card title="2. Mint" description="The  BetSell minting process will be executed, seamlessly transfroming your selected bet into a unique and certified asset in the form of an NFT. The newly minted NFT will then be allocated in the crypto wallet o your choice and automatically listed on BetSell’s Marketplace." titleColor="text-new-black" backgroundColor="#F4F4F4" image="/hiw2.png" width="70" height="70" />
         <Card title="3. Trade" description="Your minted bet is now available on the BetSell Marketplace, enabling individuals  from around the world to buy,sell and make offers on your NFT." titleColor="text-new-white" backgroundColor=" #2B213E" image="/hiw3.png" width="70" height="70" />
@@ -29,12 +29,12 @@ const Card = ({ title, backgroundImage, titleColor, description, backgroundColor
 
   return (
     <>
-      <div className="px-7 py-10 w-full h-96 3xl:h-[30rem] rounded-lg 2xl:rounded-2xl overflow-hidden flex flex-col justify-end" style={{ backgroundColor: `${backgroundColor}` }}>
+      <div className="px-7 py-10 w-full min-h-52 3xl:h-[30rem] rounded-lg 2xl:rounded-2xl overflow-hidden flex flex-col justify-end" style={{ backgroundColor: `${backgroundColor}` }}>
         <div className="h-[79.8633px]">
         <Image src={image} priority={true} alt="Icon" width={width} height={height} />
         </div>
         <p className={`${titleColor}  font-tomato text-2xl 2xl:text-4xl font-semibold mt-4 mb-2 lg:mb-6 xl:mt-8`}>{title}</p>
-        <p className={`${titleColor} text-xs sm:text-base 2xl:text-xl 3xl:text-2xl font-normal h-28 md:h-44 lg:h-32 xl:h-28 3xl:h-32`}>{description}</p>
+        <p className={`${titleColor} text-sm sm:text-base 2xl:text-xl 3xl:text-2xl font-normal min-h-28 md:h-44 lg:h-32 xl:h-28 3xl:h-32`}>{description}</p>
       </div>
     </>
 
