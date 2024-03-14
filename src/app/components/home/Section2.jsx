db08cf231bb5cc0a5144baaf156b8a8be0a2bcef
gitguti <gitmelgtrrz1@gmail.com>
2024-03-13 22:09:51 -0400

    intersection

--- src/app/components/home/Section2.jsx
+++ src/app/components/home/Section2.jsx
@@ -17,7 +17,9 @@ const Section2 = () => {
           }
         });
       },
-      { threshold: 0.5 }
+      { threshold: 0.51,
+  
+       }
     );
       console.log()
     if (ref.current) {
@@ -32,8 +34,8 @@ const Section2 = () => {
   }, []);
 
   return (
-    <div className="bg-lila">
-      <div ref={ref} className="bg-lila px-px sm:px-8 md:px-0 py-12 lg:py-24 4xl:py-36 mx-auto w-11/12 4xl:w-9/12">
+    <div className="bg-lila" ref={ref}>
+      <div className="bg-lila px-px sm:px-8 md:px-0 py-12 lg:py-24 4xl:py-36 mx-auto w-11/12 4xl:w-9/12">
         <div className="mb-6 md:mb-8 xl:mb-12 3xl:mb-10 4xl:mb-20 flex items-start md:items-center">
           <SwitchHorizontal />
           <p className="pl-4 sm:text-xs md:text-base lg:text-2xl xl:text-[1.8rem] 3x:text-[2.3rem] 4xl:text-[2.5rem] text-new-black font-tomato inline-block align-left md:align-center font-semibold">Securely trade your bets with other users</p>
